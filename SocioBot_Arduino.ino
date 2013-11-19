ac643ad438ececbb47f0d81c566af4eaf73553dc
rmoos <rmoos03@gmail.com>
2013-11-19 09:19:00 -0700

    bluetooth with commands to raise arms

--- SocioBot_Arduino.ino
+++ SocioBot_Arduino.ino
@@ -1,24 +1,47 @@
-#include <Servo.h> 
+
+// Bluetooth Serial Communication
+// Arduino Uno
+#include <Servo.h>
+#define LED 12
+
+Servo myservo; 
+Servo myservo2;
+int pos = 0;
+char val;
+
+void setup(){
  
-Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
- 
-int pos = 0;    // variable to store the servo position 
- 
-void setup() { 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
-} 
- 
- 
-void loop() { 
-  for(pos = 0; pos < 180; pos += 1) {// goes from 0 degrees to 180 degrees 
-                                     // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
+ myservo.attach(10);
+ myservo2.attach(11);
+ Serial.begin(115200);
+}
+
+void loop(){
+  Serial.println("Bluetooth");
+  val = Serial.read();
+  Serial.println(val); // Default val = ÿ
+    
+  if (val == '1'){
+         for(pos = 0; pos < 180; pos += 10)  // goes from 0 degrees to 180 degrees 
+      {                                  // in steps of 1 degree 
+        myservo.write(pos);         // tell servo to go to position in variable 'pos' 
+        myservo2.write(pos); 
+        delay(15);                       // waits 15ms for the servo to reach the position 
+      } 
+     
+     delay(1000);
   } 
-  
-  for(pos = 180; pos>=1; pos-=1) {   // goes from 180 degrees to 0 degrees                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
+  else if (val == '0'){
+         for(pos = 180; pos>=1; pos-=10) {    // goes from 180 degrees to 0 degrees                           
+          myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+          myservo2.write(pos);
+          delay(15);                       // waits 15ms for the servo to reach the position 
+          }
+     
+     delay(1000);
   } 
-} 
+
+  else {
+    delay(1000);
+  }
+}
