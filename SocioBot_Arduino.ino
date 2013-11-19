19c1fda234f6da0642ea3de3fc0cfe5f44653229
Kelton Manzanares <kmanzana@mines.edu>
2013-11-19 10:25:54 -0700

    cleaned up the code a bit

--- SocioBot_Arduino.ino
+++ SocioBot_Arduino.ino
@@ -1,47 +1,42 @@
-
-// Bluetooth Serial Communication
-// Arduino Uno
 #include <Servo.h>
+
 #define LED 12
 
-Servo myservo; 
+Servo myservo;
 Servo myservo2;
 int pos = 0;
 char val;
 
 void setup(){
- 
- myservo.attach(10);
- myservo2.attach(11);
- Serial.begin(115200);
+  myservo.attach(10);
+  myservo2.attach(11);
+
+  Serial.begin(115200);
 }
 
 void loop(){
   Serial.println("Bluetooth");
+
   val = Serial.read();
   Serial.println(val); // Default val = ÿ
-    
-  if (val == '1'){
-         for(pos = 0; pos < 180; pos += 10)  // goes from 0 degrees to 180 degrees 
-      {                                  // in steps of 1 degree 
-        myservo.write(pos);         // tell servo to go to position in variable 'pos' 
-        myservo2.write(pos); 
-        delay(15);                       // waits 15ms for the servo to reach the position 
-      } 
-     
-     delay(1000);
-  } 
-  else if (val == '0'){
-         for(pos = 180; pos>=1; pos-=10) {    // goes from 180 degrees to 0 degrees                           
-          myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-          myservo2.write(pos);
-          delay(15);                       // waits 15ms for the servo to reach the position 
-          }
-     
-     delay(1000);
-  } 
 
-  else {
+  if (val == '1'){
+    for(pos = 0; pos < 180; pos += 10) {
+        myservo.write(pos);
+        myservo2.write(pos);
+        delay(15);
+    }
+
+    delay(1000);
+  } else if (val == '0') {
+    for(pos = 180; pos>=1; pos-=10) {
+      myservo.write(pos);
+      myservo2.write(pos);
+      delay(15);
+    }
+
+    delay(1000);
+  } else {
     delay(1000);
   }
 }
